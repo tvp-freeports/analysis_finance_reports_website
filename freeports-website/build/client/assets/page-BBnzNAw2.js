05a669bf6dedb0cc5177abbd03372f5d481e050e
ManfrediO <manfredi.orlando99@gmail.com>
2025-11-13 23:32:55 +0100

    Deployed latest temporary version of the website

---
+++ freeports-website/build/client/assets/page-BBnzNAw2.js
@@ -0,0 +1,28 @@
+import{o as e,N as t}from"./chunk-D4RADZKF-af7J5MXj.js";function l(){return e.jsxs("header",{className:"w-full flex flex-col bg-black items-center py-4",children:[e.jsx("div",{to:"/",className:"hidden sm:block",children:e.jsx("img",{alt:"Desktop version of Freeports fabulous logo",className:"w-60 md:w-72 lg:w-80 mx-auto my-6",src:"/assets/logo/complete.svg"})}),e.jsx("div",{to:"/",className:"sm:hidden",children:e.jsx("img",{alt:"Mobile version of Freeports fabulous logo",className:"w-40 my-6",src:"/assets/logo/square.svg"})}),e.jsx("nav",{className:"mt-2 w-full flex justify-center",children:e.jsx("ul",{className:"flex flex-wrap justify-between w-4/5 gap-x-4 gap-y-2",children:[{path:"/",label:"Home"},{path:"/tools",label:"Resources"},{path:"/projects",label:"Projects"},{path:"/contribute",label:"Contribute"},{path:"/community",label:"Community"},{path:"/contact-us",label:"Contact Us"}].map(({path:s,label:r})=>e.jsx("li",{children:e.jsx(t,{to:s,className:"navigation-link-header",children:r})},s))})})]})}function o(){return e.jsxs("footer",{className:`\r
+        header-footer\r
+        grid\r
+        grid-cols-1\r
+        sm:grid-cols-3\r
+        items-center\r
+        overflow-visible\r
+        py-6\r
+      `,children:[e.jsxs("ul",{className:"row-start-1 col-1 justify-center sm:justify-start flex flex-row gap-6 items-center",children:[e.jsx("li",{children:e.jsx("a",{href:"https://github.com/tvp-freeports/analysis_finance_reports",target:"_blank",rel:"noopener noreferrer",children:e.jsx("img",{className:"w-16",src:"assets/github-logo/github-mark.svg",alt:"GitHub"})})}),e.jsx("li",{children:e.jsx("a",{href:"https://github.com/tvp-freeports/analysis_finance_reports",target:"_blank",rel:"noopener noreferrer",children:e.jsx("img",{className:"w-16",src:"assets/telegram-logo.svg",alt:"Telegram"})})}),e.jsx("li",{children:e.jsx("a",{href:"https://discord.gg/FRcSBWtcDR",target:"_blank",rel:"noopener noreferrer",children:e.jsx("img",{className:"w-16",src:"assets/discord-logo.svg",alt:"Discord"})})})]}),e.jsx("nav",{className:"mt-6 sm:mt-0 sm:ml-10 lg:ml-0 col-2 lg:col-3 flex flex-row gap-6 items-center justify-center sm:justify-end",children:[{path:"/contribute",label:"Join us"},{path:"/contact-us",label:"Reach out"}].map(({path:s,label:r})=>e.jsx(t,{to:s,className:`
+              relative
+              text-[#030712]
+              font-medium
+              first-letter:capitalize
+              before:block
+              before:absolute
+              before:bottom-0
+              before:left-1/2
+              before:w-0
+              before:h-[2px]
+              before:bg-[#030712]
+              before:origin-center
+              before:transition-all
+              before:duration-300
+              hover:before:w-full
+              hover:before:left-0
+              transition-colors
+              duration-300
+            `,children:r},s))})]})}function i({children:s}){return e.jsxs(e.Fragment,{children:[e.jsx(l,{}),e.jsx("main",{className:"flex-grow h-fit grid grid-cols-5 gap-10 items-center w-full",children:s}),e.jsx(o,{})]})}export{i as P};
---
+++ freeports-website/build/client/assets/with-props-BdOAGcDU.js
@@ -0,0 +1,1 @@
+import{v as p,w as t,x as e,y as o,a as s,z as u}from"./chunk-D4RADZKF-af7J5MXj.js";function i(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),matches:p()};return s.createElement(a,r)}}function m(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),error:u()};return s.createElement(a,r)}}export{m as a,i as w};
